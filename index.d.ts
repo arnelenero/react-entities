39bb6ed704d026e36e3f30ba9dd19e97b3b8d159
Arnel Enero <aenero@gmail.com>
2020-10-15 05:53:22 +0800

    TS: Better inference of expected dependency type of an entity

--- index.d.ts
+++ index.d.ts
@@ -26,34 +26,34 @@ export interface Entity<S = object, A = Actions> {
   reset: () => void;
 }
 
-export type ActionComposer<S = object, A = Actions> = (
+export type ActionComposer<S = object, A = Actions, D = any> = (
   entity: Entity<S, A>,
-  deps?: any
+  deps?: D
 ) => Action;
 
-export type ActionComposers<S = object, A = Actions> = {
-  [action in keyof A]: ActionComposer<S, A>;
+export type ActionComposers<S = object, A = Actions, D = any> = {
+  [action in keyof A]: ActionComposer<S, A, D>;
 };
 
-export interface EntityDefinition<S = object, A = Actions> {
+export interface EntityDefinition<S = object, A = Actions, D = any> {
   initialState?: S;
   options?: EntityOptions<S>;
-  [key: string]: S | EntityOptions<S> | ActionComposer<S, A>;
+  [key: string]: S | EntityOptions<S> | ActionComposer<S, A, D>;
 }
 
-export type EntityHook<S, A> = (
+export type EntityHook<S = object, A = Actions> = (
   selector?: (state: S) => any,
   equalityFn?: (a: any, b: any) => boolean
 ) => [S, A];
 
-export function createEntity<S = object, A = Actions>(
-  definition: EntityDefinition<S, A>,
-  deps?: any
+export function createEntity<S = object, A = Actions, D = any>(
+  definition: EntityDefinition<S, A, D>,
+  deps?: D
 ): Entity<S, A>;
 
-export function makeEntity<S = object, A = Actions>(
-  definition: EntityDefinition<S, A>,
-  deps?: any
+export function makeEntity<S = object, A = Actions, D = any>(
+  definition: EntityDefinition<S, A, D>,
+  deps?: D
 ): EntityHook<S, A>;
 
 export function useEntityBoundary(): void;
